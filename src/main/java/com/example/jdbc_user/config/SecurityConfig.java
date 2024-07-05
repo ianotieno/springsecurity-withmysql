da999efdd73ceeca764f49a7884b5f5d6fd18754
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-07-05 08:27:28 +0300

    update

--- src/main/java/com/example/jdbc_user/config/SecurityConfig.java
+++ src/main/java/com/example/jdbc_user/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.jdbc_user.config;
 
 import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,6 +11,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,15 +19,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-@Bean 
-InMemoryUserDetailsManager users(){
-    return new InMemoryUserDetailsManager(
-     User.withUsername("ian")
-         .password("{noop}test")
-         .roles("ADMIN")
-         .build()
-    ); 
+//@Bean 
+//InMemoryUserDetailsManager users(){
+   // return new InMemoryUserDetailsManager(
+     //User.withUsername("ian")
+      //   .password("{noop}test")
+       //  .roles("ADMIN")
+       //  .build()
+    //); 
+//}
+   @Bean
+JdbcUserDetailsManager users(DataSource dataSource){
+    JdbcUserDetailsManager jdbcUserDetailsManager= new JdbcUserDetailsManager(dataSource);
+    return  jdbcUserDetailsManager;
 }
+
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
